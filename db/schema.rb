5419b24ee019f44906673f08e6f6af4e97281813
chetana0707 <chetana0707@yahoo.co.in>
2012-06-18 23:42:09 -0400

    adding again

--- db/schema.rb
+++ db/schema.rb
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(:version => 20120617070446) do
     t.datetime "created_at",      :null => false
     t.datetime "updated_at",      :null => false
     t.string   "password_digest"
+    t.string   "remember_token"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
